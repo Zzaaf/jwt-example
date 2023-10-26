4272c003d140969cb5fd488172e013d66a56ae8b
Yuriy Pliskovsky <zzaafamania@gmail.com>
2023-10-26 09:37:10 +0300

    Added IDOR when deleting a user

--- routes/api/users.api.routes.js
+++ routes/api/users.api.routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const cookiesConfig = require('../../config/cookiesConfig');
 const { User } = require('../../db/models');
+const { verifyAccessToken } = require('../../middleware/verifyTokens');
 
 router.route('/:id')
   .put((req, res) => {
@@ -10,21 +11,25 @@ router.route('/:id')
       .then((updatedUser) => res.status(201).json(updatedUser))
       .catch((error) => res.status(500).json({ error }));
   })
-  .delete((req, res) => {
+  .delete(verifyAccessToken, (req, res) => {
     const { id } = req.params;
 
-    User.destroy({ where: { id } })
-      .then((deletedUser) => {
-        if (deletedUser) {
-          res.locals.user = {};
-          return res
-            .clearCookie(cookiesConfig.refresh)
-            .clearCookie(cookiesConfig.access)
-            .json({ delete: true, url: '/' });
-        }
-        return res.sendStatus(404);
-      })
-      .catch((error) => res.status(500).json({ error: error.message }));
+    if (res.locals.user.id === Number(id)) {
+      User.destroy({ where: { id } })
+        .then((deletedUser) => {
+          if (deletedUser) {
+            res.locals.user = {};
+            return res
+              .clearCookie(cookiesConfig.refresh)
+              .clearCookie(cookiesConfig.access)
+              .json({ delete: true, url: '/' });
+          }
+          return res.sendStatus(404);
+        })
+        .catch((error) => res.status(500).json({ error: error.message }));
+    } else {
+      return res.json({ delete: false });
+    }
   });
 
 module.exports = router;
